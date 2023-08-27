0539bcd8ef8ab0ed8a86eab7329d3b87a062c11a
silvia-vicentini <143335928+silvia-vicentini@users.noreply.github.com>
2023-08-27 17:34:51 +0200

    Update epidemic.test.cpp

--- epidemic.test.cpp
+++ epidemic.test.cpp
@@ -3,3 +3,9 @@
 
 #include "doctest.h"
 
+TEST_CASE("Not accettable values of beta and gamma") {
+  SUBCASE("Negative beta throws") { CHECK_THROWS(Epidemic{-0.3, 0.5}); }
+  SUBCASE("Negative gamma throws") { CHECK_THROWS(Epidemic{0.5, -0.3}); }
+  SUBCASE("beta > 1") { CHECK_THROWS(Epidemic{1.5, 0.5}); }
+  SUBCASE("beta > 1") { CHECK_THROWS(Epidemic{0.5, 1.5}); }
+}
