926b0129c45a95c56802f92672d2a9bf6d356559
silvia-vicentini <143335928+silvia-vicentini@users.noreply.github.com>
2023-08-28 18:21:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -35,3 +35,28 @@ int main() {
           std::cout << population_state_[i].S << "  " << population_state_[i].I
                     << "  " << population_state_[i].R << '\n';
         }
+
+  // lettura parametri da standard input
+
+        if (cmd == 'i') {  // come si mettono i const?
+          std::cout << "Please write epidemic's parameter beta and gamma\n";
+          double beta{}, gamma{};
+          std::cin >> beta >> gamma;
+          std::cout << "Please write initial population's groups S, I, R\n";
+          int S{}, I{}, R{};
+          std::cin >> S >> I >> R;
+          std::cout << "Please write the duration of the epidemic\n";
+          int time{};
+          std::cin >> time;
+          Epidemic epidemic{beta, gamma};
+          Population initial_population{S, I, R};
+          epidemic.evolve(initial_population, time);
+          auto population_state_{epidemic.state()};
+
+          std::cout << "Report for each of the stored states of population:\n";
+          std::cout << "  S  : I : R :\n";
+          for (int i{0}; i <= time; ++i) {
+            std::cout << population_state_[i].S << "  "
+                      << population_state_[i].I << "  "
+                      << population_state_[i].R << '\n';
+          }˙
