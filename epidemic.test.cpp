3012f7777c251f24c912a2a0967308dd52cf8eda
silvia-vicentini <143335928+silvia-vicentini@users.noreply.github.com>
2023-08-28 23:40:11 +0200

    Update epidemic.test.cpp

--- epidemic.test.cpp
+++ epidemic.test.cpp
@@ -8,31 +8,30 @@ bool result(Population const& p, Population const& v) {
 }
 
 TEST_CASE("Testing the Epidemic development") {
-  SUBCASE("Negative beta throws") { CHECK_THROWS(Epidemic{-0.3, 0.5}); }     //ci sono altre eccezioni da tenere in considerazione?
+  SUBCASE("Negative beta throws") { CHECK_THROWS(Epidemic{-0.3, 0.5}); }
   SUBCASE("Negative gamma throws") { CHECK_THROWS(Epidemic{0.5, -0.3}); }
   SUBCASE("beta > 1") { CHECK_THROWS(Epidemic{1.5, 0.5}); }
   SUBCASE("gamma > 1") { CHECK_THROWS(Epidemic{0.5, 1.5}); }
   SUBCASE("beta < gamma") { CHECK_THROWS(Epidemic{0.5, 0.8}); }
   SUBCASE("beta = gamma") { CHECK_THROWS(Epidemic{0.5, 0.5}); }
 
-  Epidemic epidemic{0.8, 0.7};
-
   SUBCASE("Normal use, N=300") {
+    Epidemic epidemic{0.8, 0.7};
     Population initial_population{299, 1, 0};
-    epidemic.evolve(initial_population, 4);
-    const auto state = epidemic.state();
+    int const time{4};
+    const auto state = epidemic.evolve(initial_population, time);
     CHECK(result(state[0], {299, 1, 0}));
     CHECK(result(state[1], {298, 1, 1}));
     CHECK(result(state[2], {297, 1, 2}));
     CHECK(result(state[3], {296, 1, 3}));
     CHECK(result(state[4], {295, 1, 4}));
   }
-  
-  SUBCASE("Normal use, N=1000") {
+
+  SUBCASE("Normal use, N=300") {
     Epidemic epidemic{0.8, 0.2};
     Population initial_population{990, 10, 0};
-    epidemic.evolve(initial_population, 4);
-    const auto state = epidemic.state();
+    int const time{4};
+    const auto state = epidemic.evolve(initial_population, time);
     CHECK(result(state[0], {990, 10, 0}));
     CHECK(result(state[1], {982, 16, 2}));
     CHECK(result(state[2], {970, 25, 5}));
