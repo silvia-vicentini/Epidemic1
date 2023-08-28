3817e31ebb642749f831144c23ecbc3ef092329a
silvia-vicentini <143335928+silvia-vicentini@users.noreply.github.com>
2023-08-28 12:40:28 +0200

    Update epidemic.hpp

--- epidemic.hpp
+++ epidemic.hpp
@@ -1,19 +1,18 @@
 #ifndef EPIDEMIC_HPP  // verifica tutti i const e i &&
 #define EPIDEMIC_HPP
 
-#include <cmath>  //R0 del testo è diverso rispetto al rapporto beta/gamma? perchè altrimenti i parametri sono meno di 5
+#include <cmath>  
 #include <vector>
 
-struct Population {  // è giusta questa divisione tra struct e class o
-                     // vanno scambiati? Devo inserire un'altra class?
+struct Population {  // è sensata questa divisione tra struct e class?
   int S{};
   int I{};
   int R{};
 };
 
-class Epidemic {
+class Epidemic { //verifica tutti i const
  private:
-  double const beta_;  // devono essere const? devo usare & o * ?
+  double const beta_;
   double const gamma_;
   std::vector<Population> population_state_;
 
@@ -21,13 +20,11 @@ class Epidemic {
 
  public:
   Epidemic(double const,
-           double const);  // è giusto const qui? devo mettere anche la
-                           // popolazione iniziale tra le variabili?
+           double const);
 
-  std::size_t size() const;  // non so se serva tato che deve essere uguale a t
+  std::size_t size() const;  // <-- non so se serva dato che deve essere uguale a t
 
-  void push_back(Population &);  // per inserire ogni step
-                                 // dell'evoluzione della pandemia
+  void push_back(Population &);  // serve? serve il &?
 
   void evolve(Population &,
               int const);  // questa funzione consente di calcolare l'evoluzione
