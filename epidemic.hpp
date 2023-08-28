0e2b5ba07a3e78d4641c124d71f845acdd286fd4
silvia-vicentini <143335928+silvia-vicentini@users.noreply.github.com>
2023-08-28 12:42:39 +0200

    Update epidemic.hpp

--- epidemic.hpp
+++ epidemic.hpp
@@ -22,10 +22,6 @@ class Epidemic { //verifica tutti i const
   Epidemic(double const,
            double const);
 
-  std::size_t size() const;  // <-- non so se serva dato che deve essere uguale a t
-
-  void push_back(Population &);  // serve? serve il &?
-
   void evolve(Population &,
               int const);  // questa funzione consente di calcolare l'evoluzione
                            // della popolazione ad un istante di tempo t
