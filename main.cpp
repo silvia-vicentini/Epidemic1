9bc7940529095f4db72762ca5f5b55f258791cb2
matildapasquini1 <143339525+matildapasquini1@users.noreply.github.com>
2023-08-29 11:57:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,29 @@
-int main()˙{}
+int main()˙{
+  // grafico
+sf::RenderWindow window((800, 600), "Epidemic evolution");
+std::ifstream infile (" ");// metti tra virgolette nome del file da cui prendi dati
+if (!infile) { 
+    throw std::runtime_error {"Impossible to open file"}
+}
+
+std::vector<int> time;
+std::vector<int> population_state; //oppure devoinserire singolarmente S,I e R???
+
+sf::VertexArray infectionCurve(sf::LineStrip);
+sf::VertexArray recoveryCurve(sf::LineStrip);
+sf:VertexArray susceptibleCurve(sf::LineStrip);
+
+while (window.isOpen())
+{
+     sf::Event event;
+        while (window.pollEvent(event))
+        {if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+
+window.draw(infectionCurve);
+window.draw(recoveryCurve);
+window.draw(susceptibleCurve);
+}
+}
