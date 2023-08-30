ad8061f19f7a0c4c315d365f982c5f6003dfd8d6
silvia-vicentini <143335928+silvia-vicentini@users.noreply.github.com>
2023-08-30 22:07:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 
+
 #include <algorithm>  //serve?
 #include <fstream>    // per leggere da file in input
 #include <iomanip>    //serve?
@@ -7,39 +8,40 @@
 
 #include "epidemic.hpp"
 
+namespace pf {
 struct Reading {  // va messo qui o nell'header file
   double beta{}, gamma{};
   int S{}, I{}, R{};
-  int time{};
+  int day{};
 };
 
 int main() {
   std::cout << "Valid commands: \n"
-            << "- read data from a sample file [f FILE_NAME]\n"
-            << "- read data from standard input [i]\n"
             << "- read data from command line [l]\n"
+            << "- read data from standard input [i]\n"
+            << "- read data from a sample file [f FILE_NAME]\n"
             << "- quit [q]\n";
   char cmd;
   while (std::cin >> cmd) {
-    // prende da command line con popolazione iniziale S = 997, I = 3, R = 0,
-    // Parametri della pandemia beta = 0.8, gamma = 0.4, per un arco di tempo
-    // time = 7 giorni.
+    // takes the parameters from command line, beta = 0.8, gamma = 0.4, S =
+    // 997, I = 3, R = 0, day = 40.
     if (cmd == 'l') {
       Epidemic epidemic{0.8, 0.4};
       Population initial_population{997, 3, 0};
-      int const time{7};
-      auto population_state_{epidemic.evolve(initial_population, time)};
+      int const day{40};
+      auto population_state_{epidemic.evolve(initial_population, day)};
 
       std::cout << "Report for each of the stored states of population:\n";
-      std::cout << "S    I   R \n";
-      for (int i{0}; i <= time; ++i) {
-        std::cout << population_state_[i].S << "  " << population_state_[i].I
-                  << "  " << population_state_[i].R << '\n';
+      std::cout << "Day  S    I   R \n";
+      for (int i{0}; i <= day; ++i) {
+        std::cout << i << population_state_[i].S << "  "
+                  << population_state_[i].I << "  " << population_state_[i].R
+                  << '\n';
       }
       std::cout << "To print che graphic to screen use command [g]\n";
       std::cin >> cmd;
       if (cmd == 'g') {
-        epidemic.graph(time, initial_population);
+        epidemic.graph(day, population_state_);
       } else {
         std::cout << "To run che program again press one of the commands "
                      "above, otherwise press q\n";
@@ -56,23 +58,24 @@ int main() {
       int S{}, I{}, R{};
       std::cin >> S >> I >> R;
       std::cout << "Please write the duration of the epidemic in days:\n";
-      int time{};
-      std::cin >> time;
+      int day{};
+      std::cin >> day;
       Epidemic epidemic{beta, gamma};
       Population initial_population{S, I, R};
-      auto population_state_{epidemic.evolve(initial_population, time)};
+      auto population_state_{epidemic.evolve(initial_population, day)};
 
       std::cout << "Report for each of the stored states of population:\n";
-      std::cout << "S    I   R \n";
-      for (int i{0}; i <= time; ++i) {
-        std::cout << population_state_[i].S << "  " << population_state_[i].I
-                  << "  " << population_state_[i].R << '\n';
+      std::cout << "Day  S    I   R \n";
+      for (int i{0}; i <= day; ++i) {
+        std::cout << i << population_state_[i].S << "  "
+                  << population_state_[i].I << "  " << population_state_[i].R
+                  << '\n';
       }
       std::cout << "To print che graphic to screen use command [g]\n";
       std::cin >> cmd;
       if (cmd == 'g') {
-        epidemic.graph(time,
-                       initial_population);  // funzione ancora da definire
+        epidemic.graph(day,
+                       population_state_);  // funzione ancora da definire
       } else {
         std::cout << "To run che program again press one of the commands "
                      "above, otherwise press q\n";
@@ -80,7 +83,7 @@ int main() {
     }
 
     // lettura dei parametri da file dove compaiono nel seguente ordine:
-    // beta, gamma, S, I, R, time.
+    // beta, gamma, S, I, R, day.
 
     std::string filename;
     if (cmd == 'f' && std::cin >> filename) {
@@ -90,34 +93,35 @@ int main() {
       } else {                                                 // è necessario?
         double beta_, gamma_;
         int S_, I_, R_;
-        int time_;
+        int day_;
         Reading parameters{};  // uso una struct e non un vett perchè alcuni
                                // elementi sono int altri double
-        while (infile >> beta_ >> gamma_ >> S_ >> I_ >> R_ >> time_) {
+        while (infile >> beta_ >> gamma_ >> S_ >> I_ >> R_ >> day_) {
           parameters.beta = beta_;
           parameters.gamma = gamma_;
           parameters.S = S_;
           parameters.I = I_;
           parameters.R = R_;
-          parameters.time = time_;  // si può scrivere in maniera più rapida?
+          parameters.day = day_;  // si può scrivere in maniera più rapida?
         }
         std::cout << "Input file read successfully" << '\n';  // lo mettiamo?
         Epidemic epidemic{parameters.beta, parameters.gamma};
         Population initial_population{parameters.S, parameters.I, parameters.R};
         auto population_state_{
-            epidemic.evolve(initial_population, parameters.time)};
+            epidemic.evolve(initial_population, parameters.day)};
 
         std::cout << "Report for each of the stored states of population:\n";
-        std::cout << "S    I   R \n";
-        for (int i{0}; i <= parameters.time; ++i) {
-          std::cout << population_state_[i].S << "  " << population_state_[i].I
-                    << "  " << population_state_[i].R << '\n';
+        std::cout << "Day  S    I   R \n";
+        for (int i{0}; i <= parameters.day; ++i) {
+          std::cout << i << population_state_[i].S << "  "
+                    << population_state_[i].I << "  " << population_state_[i].R
+                    << '\n';
         }
         std::cout << "To print che graphic to screen use command [g]\n";
         std::cin >> cmd;
         if (cmd == 'g') {
-          epidemic.graph(parameters.time,
-                         initial_population);  // funzione ancora da definire
+          epidemic.graph(parameters.day,
+                         population_state_);  // funzione ancora da definire
         } else {
           std::cout << "To run che program again press one of the commands "
                        "above, otherwise press q\n";
@@ -133,7 +137,7 @@ int main() {
      std::ofstream outfile{"results.txt"};
 
      if (!outfile) {
-       throw std::runtime_error{"Impossible to open file!"};
+       throw std::runday_error{"Impossible to open file!"};
      }
      outfile << "- Read data:" << data.size() << '\n';
      const auto stat = data.statistics();
@@ -155,3 +159,4 @@ int main() {
     }
   }
 }
+}  // namespace pf
