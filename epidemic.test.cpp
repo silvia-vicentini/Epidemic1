81986e768af5036ceed7303e497980c9cd1b2b4d
silvia-vicentini <143335928+silvia-vicentini@users.noreply.github.com>
2023-08-30 22:07:33 +0200

    Update epidemic.test.cpp

--- epidemic.test.cpp
+++ epidemic.test.cpp
@@ -3,6 +3,7 @@
 
 #include "doctest.h"
 
+namespace pf {
 bool result(Population const& p, Population const& v) {
   return p.S == v.S && p.I == v.I && p.R == v.R;
 }
@@ -16,26 +17,33 @@ TEST_CASE("Testing the Epidemic development") {
   SUBCASE("beta = gamma") { CHECK_THROWS(Epidemic{0.5, 0.5}); }
 
   SUBCASE("Normal use, N=300") {
-    Epidemic epidemic{0.8, 0.7};
+    Epidemic epidemic{0.8, 0.79};
     Population initial_population{299, 1, 0};
-    int const time{4};
+    int const time{15};
     const auto state = epidemic.evolve(initial_population, time);
     CHECK(result(state[0], {299, 1, 0}));
     CHECK(result(state[1], {298, 1, 1}));
     CHECK(result(state[2], {297, 1, 2}));
-    CHECK(result(state[3], {296, 1, 3}));
-    CHECK(result(state[4], {295, 1, 4}));
+    CHECK(result(state[3], {297, 1, 2}));
+    CHECK(result(state[4], {296, 1, 3}));
+    CHECK(result(state[13], {289, 1, 10}));
+    CHECK(result(state[14], {288, 1, 11}));
+    CHECK(result(state[15], {287, 1, 12}));
   }
 
   SUBCASE("Normal use, N=300") {
-    Epidemic epidemic{0.8, 0.2};
-    Population initial_population{990, 10, 0};
-    int const time{4};
+    Epidemic epidemic{0.99, 0.01};
+    Population initial_population{299, 1, 0};
+    int const time{15};
     const auto state = epidemic.evolve(initial_population, time);
-    CHECK(result(state[0], {990, 10, 0}));
-    CHECK(result(state[1], {982, 16, 2}));
-    CHECK(result(state[2], {970, 25, 5}));
-    CHECK(result(state[3], {951, 39, 10}));
-    CHECK(result(state[4], {921, 61, 18}));
+    CHECK(result(state[0], {299, 1, 0}));
+    CHECK(result(state[1], {298, 2, 0}));
+    CHECK(result(state[2], {296, 4, 0}));
+    CHECK(result(state[3], {293, 7, 0}));
+    CHECK(result(state[4], {286, 13, 1}));
+    CHECK(result(state[13], {1, 190, 109}));
+    CHECK(result(state[14], {0, 172, 128}));
+    CHECK(result(state[15], {0, 155, 145}));
   }
 }
+}  // namespace pf
