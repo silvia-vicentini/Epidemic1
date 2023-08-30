8e500e54e634e0cd88256081a8cade4cebeefd2c
silvia-vicentini <143335928+silvia-vicentini@users.noreply.github.com>
2023-08-30 23:59:10 +0200

    Update epidemic.hpp

--- epidemic.hpp
+++ epidemic.hpp
@@ -5,7 +5,7 @@
 #include <vector>
 
 // bisogna modificare i throw con gli assert
-//bisogna confermare o correggere tutti i const e & 
+// bisogna confermare o correggere tutti i const e &
 // bisogna scrivere i commenti dove necessario
 
 namespace pf {
@@ -21,17 +21,23 @@ class Epidemic {
   double const gamma_;
   std::vector<Population> population_state_;
 
-  Population solve(Population const, int const) const;
+  Population solve(Population const, int const);
 
  public:
   Epidemic(double const, double const);
 
+  Population approx(Population, int const);  // per fare le approssimazioni
+
+  Population correcting(Population,
+                        int const);  // per approssimare in base alle nostre
+                                     // approssimazioni population_state;
+
   std::vector<Population> evolve(
-      Population,
+      Population const,
       int const);  // questa funzione consente di calcolare l'evoluzione
                    // della popolazione ad un istante di tempo t
- 
-  void graph(int, std::vector<Population>);
+
+  // void graph(int, std::vector<Population>);
 
   /*std::vector<Population> const &state()
       const;  // questa funzione restituisce lo stato della popolazione*/
