29950bf32375511341748a890868bc0deea6cdc2
matildapasquini1 <143339525+matildapasquini1@users.noreply.github.com>
2023-09-01 01:06:13 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 #include <vector>
 
-int main()˙{
-  // grafico
-sf::RenderWindow window((800, 600), "Epidemic evolution"); //crea finestra grafica
+int main(){
+  // GRAFICO
+sf::RenderWindow window(sf::VideoMode(800, 600), "Epidemic evolution"); //crea finestra grafica
 
-  sf::RectangleShape xAxis (sf::Vector2f (window.getSize().x, 2.f); //crea asse x che ha dimensioni: lungo quanto la lunghezza della finestra e alto due unità
+  sf::RectangleShape xAxis (sf::Vector2f (window.getSize().x, 2.f)); //crea asse x che ha dimensioni: lungo quanto la lunghezza della finestra e alto due unità
  xAxis.setPosition(0.f, window.getSize().y/2); // imposta posizione dell'asse x. posizionato all'inizio della finestra e a metà altezza della finestra
   xAxis.setFillColor (sf::Color::Black); // asse x di colore nero
   
@@ -22,8 +22,11 @@ sf::Font font;
   xAxisName.setPosition (window.getSize().x -100, window.getSize().y - 40); //non ho la minima idea di dove lo abbia posizionato,controlla!!!
   yAxisName.setPosition (20,20) //non ho la minima idea di dove lo abbia posizionato,controlla!!!
 
-  float maxXValue =  //stabilisci quale è il valore max sull'asse delle x
-  float maxYValue =  //stabilisci quale è il valore max sull'asse delle y
+ 
+  sf::Vector2f graphSize(600.f, 400.f); // i valori sono a caso
+  sf::Vector2f graphPosition(100.f,100.f); // i valori sono a caso. Definizione delle dimesioni e posizioni del grafico.
+  float maxXValue = 50; //stabilisci quale è il valore max sull'asse delle x
+  float maxYValue = 100; //stabilisci quale è il valore max sull'asse delle y
   for (float y = 0; y <= maxYValue; y += 20.f) //il valore 20.f è a caso
     { sf::Vertex line [] =  { 
      sf::Vertex (sf::Vector2f (graphPosition.x, graphPosition.y + graphSize.y -(y/ maxYvalue)* graphSize.y), sf::Color::Black ), 
@@ -39,15 +42,45 @@ sf::Font font;
      window.draw(line, 2, sf::Lines);
     }
 
+  std::vector<sf::Text> xTicks; // creazione degli oggetti di testo per le tacchette sugli assi
+  std::vector<sf::Text> yTicks;
+  for (float x=0.f; x<= maxXValue; x += 2.f) { 
+      sf::Text tickName; // o devo mettere sf::text tickLabel ???
+      tickName.setFont(font);
+      tickName.setCharacterSize(14);
+      tickName.setFillColor(sf::Color::Black);
+      tickName.setString(std::to_string(static_cast<int>(x))); //qui viene impostato il contenuto del testo dell'oggetto tickName CON IL VALORE numerico DI X.std::to_string converte il valore numerico in una stringa. static_cast<int> ci assicura che il valore di y venga convertito in un intero prima di essere convertito in stringa.
+      tickName.setPosition(graphPosition.x + (x / maxXValue) * graphSize.x - 10, graphPosition.y + graphSize.y + 10 ); //valori a caso
+      xTicks.push_back(tickName); // l'oggeto tickName viene aggiunto al vettore xTicks che conterrà tutte le etichette delle tacchette lungo l'asse delle x
+  }
+ for (float y=0.f; y<= maxXValue; y += 20.f) { 
+      sf::Text tickName; // o devo mettere sf::text tickLabel ???
+      tickName.setFont(font);
+      tickName.setCharacterSize(14);
+      tickName.setFillColor(sf::Color::Black);
+      tickName.setString(std::to_string(static_cast<int>(y))); //qui viene impostato il contenuto del testo dell'oggetto tickName CON IL VALORE numerico DI Y.std::to_string converte il valore numerico in una stringa. static_cast<int> ci assicura che il valore di y venga convertito in un intero prima di essere convertito in stringa.
+      tickName.setPosition(graphPosition.x - 30, graphPosition.y + graphSize.y - (y / maxYValue) * graphSize.y -7 ); //valori a caso
+      yTicks.push_back(tickName); // l'oggeto tickName viene aggiunto al vettore xTicks che conterrà tutte le etichette delle tacchette lungo l'asse delle x
+  }
 
+  while (window.isOpen())
+{
+     sf::Event event;
+        while (window.pollEvent(event))
+        {if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear(sf::Color::White);
 
-  
 window.draw(xAxis);
 window.draw(yAxis);
 window.draw (xAxisName);
 window.draw (yAxisName);
+for (const sf::Text& tick : xTicks) { window.draw(tick); } //ciclo range-based . Serve per disegnare l'oggetto tick (corrente) sulla finestra. Tutte le etichette vengono disegnate una dopo l'altra seguendo l'ordine in cui sono nel vettore xTicks.
+for (const sf::Text& tick : yTicks) { window.draw(tick); }
   window.display();
-  
+  }
+  }
 
   
 
