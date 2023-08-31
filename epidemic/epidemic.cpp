13494e8f5f670d86a388fc2a0cdb8f67053c4109
silvia-vicentini <143335928+silvia-vicentini@users.noreply.github.com>
2023-09-01 01:42:21 +0200

    Update epidemic.cpp

--- epidemic/epidemic.cpp
+++ epidemic/epidemic.cpp
@@ -1,16 +1,12 @@
 #include "epidemic.hpp"
 
-#include <assert.h>
-
-// #include <Graph.h>
-
-// #include <SFML/Graphics.hpp>
-// #include <SFML/System.hpp>
 #include <cmath>
-#include <stdexcept>  //per fare i throw
+#include <stdexcept>
 #include <vector>
-
+// devo definire vaccine
 namespace pf {
+
+// constructor initialization
 Epidemic::Epidemic(double const beta, double const gamma)
     : beta_{beta}, gamma_{gamma} {
   if (beta_ < 0. || beta_ > 1.) {
@@ -27,6 +23,7 @@ Epidemic::Epidemic(double const beta, double const gamma)
   }
 }
 
+// definition of solve function
 Population Epidemic::solve(Population const prev_state, int const N) {
   double const beta{Epidemic::beta_};
   double const gamma{Epidemic::gamma_};
@@ -42,6 +39,7 @@ Population Epidemic::solve(Population const prev_state, int const N) {
   return Population{S_i, I_i, R_i};
 }
 
+// definition of approx function
 Population Epidemic::approx(Population population_state, int const N) {
   int tot = population_state.S + population_state.I + population_state.R;
   if (tot != N) {
@@ -55,10 +53,41 @@ Population Epidemic::approx(Population population_state, int const N) {
   return Population{population_state.S, population_state.I, population_state.R};
 }
 
-Population Epidemic::correct(Population population_state, int const N) {
-  return Population{approx(solve(population_state, N), N)};
+// definition of lockdown function
+Population Epidemic::lockdown(Population prev_state, int const N) {
+  double const beta{Epidemic::beta_};
+  double const gamma{Epidemic::gamma_};
+  int R_i = std::round(prev_state.R + gamma * prev_state.I);
+  int I_i = N - prev_state.S - R_i;
+
+  return Population{prev_state.S, I_i, R_i};
 }
 
+/*
+// se si fanno dei vaccini parte dei sani entra nei R perché diventano immuni
+Population Epidemic::vaccine(Population prev_state, int const N,
+                             double const v) {
+  if (v < 0.) {
+    throw std::runtime_error{"Vaxination parameter v must be > 0"};
+  } else if (v > 0.1) {
+    throw std::runtime_error{
+        "Not possible to vaccine more than 10% of the population per day"};
+  } else {
+    double const beta{Epidemic::beta_};
+    double const gamma{Epidemic::gamma_};
+    int R_i =
+        std::round(prev_state.R + gamma * prev_state.I + v * prev_state.S);
+    int I_i = std::round(prev_state.I + beta * prev_state.S * prev_state.I / N -
+                         gamma * prev_state.I);
+    int S_i = std::round(prev_state.S - beta * prev_state.S * prev_state.I / N -
+                         v * prev_state.S);
+
+    return Population{prev_state.S, I_i, R_i};
+  }
+}
+*/
+
+// definition of evolve function
 std::vector<Population> Epidemic::evolve(Population initial_population,
                                          int const time) {
   population_state_.push_back(initial_population);
@@ -66,67 +95,20 @@ std::vector<Population> Epidemic::evolve(Population initial_population,
   int const N =
       initial_population.S + initial_population.I + initial_population.R;
 
-  for (int i{0}; i < time; ++i) {
-    auto next_state = correct(population_state_[i], N);
-    population_state_.push_back(next_state);
+  for (int i{0}; i < time;) {
+    if (population_state_[i].I < 0.6 * N) {
+      auto next_state = approx(solve(population_state_[i], N), N);
+      population_state_.push_back(next_state);
+      ++i;
+    } else {
+      for (int a{0}; a < 14; ++a) {
+        auto next_state = approx(lockdown(population_state_[i], N), N);
+        population_state_.push_back(next_state);
+      }
+      i += 14;
+    }
   }
   return Epidemic::population_state_;
 }
 
-/*
-
-void Epidemic::graph(int time, std::vector<Population> population_state_) {
-  // bisogna creare tre grafici da sovrapporre, uno per l'andamento di S uno
-per
-  // quello di I uno per quello di R
-  if (Epidemic::population_state_.size() < 2) {
-    throw std::runtime_error{"Not enough entries to draw a graphic"};
-  }
-
-  if (time <= 0) {
-    throw std::runtime_error{"Not enough time to draw a graphic"};
-  }
-
-  // Create the main window
-  sf::RenderWindow window(sf::VideoMode(800, 600),
-                          "Epidemic evolution");  // che libreria bisogna
-usare?
-
-  Simple_window window{100, 100, 600, 400, "Epidemic development"};
-  window.set_color(Color::white);
-  Axis time{Axis::x, Point{20, 300}, 280, 10, "time"};
-  window.attach(time);
-  Axis people{Axis::y, Point{20, 300}, 280, 10, "number of people"};
-  window.attach(people);
-
-  // Create points
-  for (int i{}; i <= time; ++i) {
-    Point S{i, population_state_[i].S};
-    S.set_color(Cotor::blue);
-    Point I{i, population_state_[i].I};
-    I.set_color(Cotor::red);
-    Point R{i, population_state_[i].R};
-    R.set_color(Cotor::green);
-    window.attach(S, I, R);
-  }
-
-  // non so se serva
-  sf::VertexArray Infection_Curve(sf::LineStrip);
-  sf::VertexArray Recovery_Curve(sf::LineStrip);
-  sf::VertexArray Susceptible_Curve(sf::LineStrip);
-
-  while (window.isOpen()) {
-    sf::Event event;
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed) window.close();
-    }
-    window.clear(sf::Color::White);
-    window.draw(Infection_Curve);
-    window.draw(Recovery_Curve);
-    window.draw(Susceptible_Curve);
-    window.display();
-  }
-}*/
 }  // namespace pf
-
-/*std::vector<Population> const &Epidemic::state() const {}*/
