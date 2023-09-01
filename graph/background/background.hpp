b437167c5dc87e8cd33a294e25a6a59db5fffb85
silvia-vicentini <143335928+silvia-vicentini@users.noreply.github.com>
2023-09-01 09:40:47 +0200

    Create background.hpp

---
+++ graph/background/background.hpp
@@ -0,0 +1,14 @@
+#ifndef BACKGROwND_HPP
+#define BACKGROwND_HPP
+
+#include <SFML/Graphics.hpp>
+
+namespace pf {
+// to create the backgrownd
+void draw_backgrownd();
+
+// to set the graph dimensions
+void set_dimensions();
+
+}  // namespace pf
+#endif
