293e3c6887fa94c3a9d8a9882bb012ff64cda469
silvia-vicentini <143335928+silvia-vicentini@users.noreply.github.com>
2023-09-01 09:41:47 +0200

    Create curves.hpp

---
+++ graph/curves/curves.hpp
@@ -0,0 +1,14 @@
+#define CURVES_HPP
+#ifndef CURVES_HPP
+
+#include <SFML/Graphics.hpp>
+
+#include "../epidemic/epidemic.hpp"
+
+namespace pf {
+
+// to make the curves
+void make_curves(std::vector<Population>);
+
+}  // namespace pf
+#endif
