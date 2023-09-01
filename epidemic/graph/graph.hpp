2d4c90edd5dc38ff269916f7196320240a983432
silvia-vicentini <143335928+silvia-vicentini@users.noreply.github.com>
2023-09-01 05:45:32 +0200

    Create graph.hpp

---
+++ epidemic/graph/graph.hpp
@@ -0,0 +1,11 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
+#include <SFML/Graphics.hpp>
+
+#include "../epidemic/epidemic.hpp"
+
+namespace pf {
+void graph(std::vector<Population>);
+}  // namespace pf
+#endif
