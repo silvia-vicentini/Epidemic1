835a081c8942d0c702f0a9f61cc6cbecc39e61d9
silvia-vicentini <143335928+silvia-vicentini@users.noreply.github.com>
2023-09-01 06:11:38 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,41 +14,18 @@ int main() {
             << "- print the graph of the variables [g]\n"
             << "- quit [q]\n";
   char in;
-  char out;
+  // char out;
   while (std::cin >> in) {
-
+    
     // simulation taking the parameters from command line: beta = 0.8, gamma =
     // 0.4, S = 997, I = 3, R = 0, day = 40.
     if (in == 'l') {
       command_line();
-
-      /////////////////////// scrivi codice
-
-      /*
-      std::cout << "To print the graphic to screen use command [g]\n";
-      std::cin >> out;
-      if (in == 'g') {
-        epidemic.graph(day, population_state_);
-      } else {
-        std::cout << "To run che program again press one of the commands above,
-      otherwise press q\n";
-      }
-      */
     }
 
     // reading parameters from standard input
     if (in == 'i') {  // come si mettono i const?
       standard_input();
-      /*
-      std::cout << "To print the graphic to screen use command [g]\n";
-            std::cin >> out;
-            if (in == 'g') {
-              epidemic.graph(day, population_state_);
-            } else {
-              std::cout << "To run che program again press one of the commands "
-                           "above, otherwise press q\n";
-            }
-      */
     }
 
     // reading parameters from file in the order:
@@ -56,16 +33,6 @@ int main() {
     std::string filename;
     if (in == 'f' && std::cin >> filename) {
       infile(filename);
-      /*
-      std::cout << "To print the graphic to screen use command [g]\n";
-      std::cin >> in;
-      /*if (in == 'g') {
-        epidemic.graph(parameters.day,
-                       population_state_);  // funzione ancora da definire
-      } else {
-        std::cout << "To run che program again press one of the commands "
-                     "above, otherwise press q\n";
-      }*/
     }
 
     /* // write results on results.txt file
